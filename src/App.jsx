9e3d7659dface27935820ba03a7130ab827b21ed
lumixAr12 <akujuaobidaniel@gmail.com>
2023-10-06 01:18:37 +0100

    third commit(updated)

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,31 @@ function App() {
   const [data, setData] = useState();
   const [weather, setweather] = useState();
   const [location, setlocation] = useState();
-  const [search, setSearch] = useState();
-  const date = new Date().toLocaleTimeString();
+  const [search, setSearch] = useState("lagos");
+  // const date = new Date().toLocaleTimeString();
+  const [date, setDate] = useState();
 
-  const handleChange=(e)=>{
-    setSearch(e.target.value)
-  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     axios
-      .get(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${search}`)
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=${
+          import.meta.env.VITE_API_KEY
+        }&q=${search}`
+      )
       .then((res) => {
         setweather(res.data.current);
         setlocation(res.data.location);
-        console.log(res.data.current.cloud);
+        console.log(res.data.location);
+        const date =
+          location?.locationtime === undefined
+            ? new Date().toLocaleTimeString().substring(0, 4)
+            : location?.localtime.substring(10);
+        setDate(date);
+        console.log(date);
       });
   }, [search]);
 
@@ -42,7 +53,9 @@ function App() {
                 d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
               />
             </svg>
-            <input className="h-8 border-b-2 bg-transparent click" onChange={handleChange}></input>
+            <input
+              className="h-8 border-b-2 bg-transparent text-sm px-2 mx-3"
+              onChange={handleChange}></input>
           </div>
         </div>
         <div className="text-left ">
@@ -56,7 +69,7 @@ function App() {
         </div>
       </nav>
       <main className="text-5xl flex flex-col gap-7 mt-[3.5em] mb-7 items-center text-center">
-        <p className="text-[4em]">{date}</p>
+        <p className="text-[4em] text-gray-900">{date}</p>
         <p className="text-[1.4em] mb-6">Good afternoon, L3inad</p>
         <p className="text-[.9em] mb-6">What is your main focus for today?</p>
         <hr className="border w-2/6" />
